1373e575a4eb43016e2327cb78b03f6b58537182
eunoia0411 <faire-un-reve@naver.com>
2022-03-31 14:49:06 +0900

    array

---
+++ 2920.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main() {
+	//int scaleArr[8] = { 1,2,3,4,5,6,7,8 };
+	//int dscaleArr[8] = { 8,7,6,5,4,3,2,1 };
+	int numArr[8] = { 0, };
+
+	for (int i = 0;i < 8;i++) {
+		scanf("%1d", &numArr[i]);
+	}//한번에 입력받아서 배열에 한개씩 저장 완료!0!
+
+	int a = 0;
+	int d = 0;
+	
+	int position = 0;
+	for (position;position < 8;position++) {
+		if ((position + 1) != numArr[position]) {
+			a++;
+			break;
+			//ascending이 아니면 a가 1
+		}
+	}
+	for (position;position < 8;position++){
+		if ((position+numArr[position])!=8) {
+			d++;
+			break;
+				//descending이 아니면 b가 1
+		}
+	}
+
+	printf("%d\n", a);
+	printf("%d\n", d);
+	
+	if (a == 0)
+		printf("ascending");
+	else if (d == 0)
+		printf("descending");
+	else if (a == 1 && d == 1)
+		printf("mixed");
+
+	return 0;
+}
---
+++ 8958.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning (disable:4996)
+int main() {
+
+    int n;
+    char s[100];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0, add = 1; 
+        scanf("%s", s); 
+
+        for (int j = 0; j < strlen(s); j++) { 
+            if (s[j] == 'O') {
+                sum += add; 
+                add++; 
+            }
+            if (s[j] == 'X') {
+                add = 1;
+            }
+        }
+        printf("%d\n", sum);
+    }
+}
